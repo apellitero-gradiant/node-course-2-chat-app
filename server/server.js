a2ede36204f20fa69448aae859d424f89e92e44e
Alexandre Pellitero Rivero <apellitero@gradiant.org>
2017-06-09 21:56:56 +0200

    Added connect and disconnect handlers

--- server/server.js
+++ server/server.js
@@ -1,13 +1,26 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public')
+const port = process.env.PORT || 3334;
 
 var app = express();
-const port = process.env.PORT || 3334;
+var server = http.createServer(app);
+var io = socketIO(server);
+var io= socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+  console.log('New user connected');
+
+  socket.on('disconnect', () => {
+    console.log('User was disconnected');
+  });
+});
+
+server.listen(port, () => {
   console.log(`Server is up on port ${port}`);
 });
