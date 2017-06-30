bbcf03f1f3491022e9c4c467038a7f07d724c101
Alexandre Pellitero Rivero <apellitero@gradiant.org>
2017-06-30 21:14:14 +0200

    Added newMessage and createMessage events

--- server/server.js
+++ server/server.js
@@ -15,10 +15,22 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
+  socket.emit('newMessage', {
+    from: 'John',
+    text: 'See you then',
+    createdAt: 123123
+  });
+
+  socket.on('createMessage', (message) => {
+    console.log('Message created', message);
+  });
+
   socket.on('disconnect', () => {
     console.log('User was disconnected');
   });
 });
+
+
 
 server.listen(port, () => {
   console.log(`Server is up on port ${port}`);
