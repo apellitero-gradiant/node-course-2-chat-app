97a4b69e42bfa2509f8c6ebba25d5701db589574
Alexandre Pellitero Rivero <apellitero@gradiant.org>
2017-07-04 19:42:16 +0200

    Added styles and improved UX for sending location

--- public/js/index.js
+++ public/js/index.js
@@ -28,11 +28,13 @@ socket.on('newLocationMessage', function (message) {
 
 jQuery('#message-form').on('submit', function (e) {
   e.preventDefault();
+
+  var messageTextbox = jQuery('[name=message]');
   socket.emit('createMessage', {
     from: 'User',
-    text: jQuery('[name=message]').val()
+    text: messageTextbox.val()
   }, function () {
-
+    messageTextbox.val('');
   });
 });
 
@@ -42,7 +44,9 @@ locationButton.on('click', function () {
     return alert('Geolocation not supported by yout browser.');
   }
 
+  locationButton.attr('disabled', 'disabled').text('Sending location...');
   navigator.geolocation.getCurrentPosition(function (position) {
+    locationButton.removeAttr('disabled').text('Send location');
     socket.emit('createLocationMessage', {
       latitude: position.coords.latitude,
       longitude: position.coords.longitude
